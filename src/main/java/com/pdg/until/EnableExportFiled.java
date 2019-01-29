11ff55eef6231ecfe10c6050c194ae99fedea10e
yc90037744 <2837424026@qq.com>
2019-01-29 09:34:00 +0800

    pdg

---
+++ src/main/java/com/pdg/until/EnableExportFiled.java
@@ -0,0 +1,14 @@
+package com.pdg.until;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnableExportFiled {
+    int colWidth() default 100;
+    String colName();
+}
+
